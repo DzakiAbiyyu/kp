cd9464543613838955bb3b8ff436d9e303d7f3c7
hafizalkariem <ahapiz95@gmail.com>
2025-04-30 01:13:43 +0700

    initial commit

--- Leuwi-Hejo/app/Controllers/pages.php
+++ Leuwi-Hejo/app/Controllers/pages.php
@@ -19,38 +19,25 @@ class Pages extends BaseController
         $ContentModel = new ContentModel();
         $konten = $ContentModel->where('slug', 'home_hero')->first();
 
-<<<<<<< HEAD
-        $data = [
-            'title' => 'Home | Leuwi Hejo',
-            'konten' => $konten
-        ];
 
-=======
 
         $data = [
             'title' => 'Home | Leuwi Hejo',
             'konten' => $konten
         ];
 
->>>>>>> 2f5f680020df4e91a8ae7f82e79136d0bd0ff7af
         return view('pages/home', $data);
     }
 
     public function about()
     {
         $ContentModel = new ContentModel();
-<<<<<<< HEAD
+
         $konten = $ContentModel->where('slug', 'siapa_kami')->first();
         $data = [
             'title' => 'About | Leuwi Hejo',
             'konten' => $konten
-=======
-        $tentang_kami = $ContentModel->where('slug', 'siapa_kami')->first();
-        $data = [
-            'title' => 'About | Leuwi Hejo',
-            'konten' => $tentang_kami
 
->>>>>>> 2f5f680020df4e91a8ae7f82e79136d0bd0ff7af
         ];
         echo view('pages/about', $data);
     }
