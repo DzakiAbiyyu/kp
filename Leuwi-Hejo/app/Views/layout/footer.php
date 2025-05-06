34b3e26c8af6999457561a42345f79d6d241c71c
hafizalkariem <ahapiz95@gmail.com>
2025-05-06 21:28:36 +0700

    fixed bug

--- Leuwi-Hejo/app/Views/layout/footer.php
+++ Leuwi-Hejo/app/Views/layout/footer.php
@@ -1,5 +1,12 @@
    <!-- Footer Start -->
-   <footer class="footer">
+   <?php
+    $uri = service('uri');
+    $path = trim($uri->getPath(), '/'); // hapus / di awal/akhir
+    $isHome = $path === '' || $path === 'index.php';
+    ?>
+
+
+   <footer class="footer <?= $isHome ? 'fixed' : '' ?>">
        <div class="footer-content">
            <p>&copy; <?= date('Y'); ?> Leuwi Hejo. Semua hak dilindungi</p>
            <div class="sosial-media">
